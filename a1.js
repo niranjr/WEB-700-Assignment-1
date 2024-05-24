dd99f3e731486bdbc3c107368bf91086a97d26bc
niranjr <170667650+niranjr@users.noreply.github.com>
2024-05-24 16:54:11 -0400

    Add files via upload

---
+++ a1.js
@@ -0,0 +1,52 @@
+/*********************************************************************************
+*  WEB700 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Nirajbhai Limbasiya Student ID: 146215231 Date: 24/05/2024
+*
+********************************************************************************/ 
+
+console.log("Hello World");
+
+// arrays for server verbs, paths, and responses
+let serverVerbs = ["GET", "GET", "GET", "POST", "GET", "POST"];
+let serverPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout"];
+let serverResponses = [
+    "Welcome to WEB700 Assignment 1",
+    "This course name is WEB700. This assignment was prepared by Nirajbhai Limbasiya",
+    "nrlimbasiya@myseneca.ca, nirajbhai limbasiya",
+    "Hello, User Logged In",
+    "Main Panel",
+    "Logout Complete. Goodbye"
+];
+
+function httpRequest(httpVerb, path) {
+    for (let i = 0; i < serverPaths.length; i++) {
+        if (serverVerbs[i] === httpVerb && serverPaths[i] === path) {
+            return `200: ${serverResponses[i]}`;
+        }
+    }
+    return `404: Unable to process ${httpVerb} request for ${path}`;
+}
+
+//function to generate a random integer
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function automateTests() {
+    const testVerbs = ["GET", "POST"];
+    const testPaths = ["/", "/about", "/contact", "/login", "/panel", "/logout", "/randomPath1", "/randomPath2"];
+
+    function randomRequest() {
+        const randVerb = testVerbs[getRandomInt(testVerbs.length)];
+        const randPath = testPaths[getRandomInt(testPaths.length)];
+        console.log(httpRequest(randVerb, randPath));
+    }
+
+    setInterval(randomRequest, 1000);
+}
+
+automateTests();
